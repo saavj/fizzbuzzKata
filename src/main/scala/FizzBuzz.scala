39170b99fa18ee02a31544063fa8796533175960
Sofia Vaughan-Jones <sofia.vaughan.jones@itv.com>
2018-06-22 15:13:47 +0100

    Deleted solution

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -1,10 +1,5 @@
 package com.sofiaa.fizzbuzz
 
 object FizzBuzz {
-  def getResult(int: Int): String = {
-    if (int % 15 == 0)     "fizzbuzz"
-    else if (int % 5 == 0) "buzz"
-    else if (int % 3 == 0) "fizz"
-    else                   int.toString
-  }
+
 }
